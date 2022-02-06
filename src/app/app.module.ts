cf1903de6a7c5c7438f04ed8203bb2bd71e44801
priscahlimo <priscahlimoh@gmail.com>
2022-02-06 12:49:39 +0300

    created home page component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuotesComponent } from './quotes/quotes.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuotesComponent
+    QuotesComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
