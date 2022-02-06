68bc4331159b324677d29cceeffe48fa8c53bc39
priscahlimo <priscahlimoh@gmail.com>
2022-02-06 12:32:42 +0300

    create a quotes class

---
+++ src/app/quotes.ts
@@ -0,0 +1,5 @@
+export class Quotes {
+    constructor(public name:string,public author:string, public description:string,public likes:number,public dislikes:number, public postedOn:Date){
+        
+    }
+}
