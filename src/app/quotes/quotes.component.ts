7f90198fa422e6a22e940b1b9c49c4467a9b24a2
priscahlimo <priscahlimoh@gmail.com>
2022-02-06 13:39:03 +0300

    installed bootstrap

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Quotes } from '../quotes';
 
 @Component({
   selector: 'app-quotes',
@@ -7,7 +8,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuotesComponent implements OnInit {
 
-  
+
 
   constructor() { }
 
