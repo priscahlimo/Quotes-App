6df42fa9a503209726f1838e7db59fcbbdd7c05a
priscahlimo <priscahlimoh@gmail.com>
2022-02-06 17:15:10 +0300

    remove card

--- src/app/quotes/quotes.component.ts
+++ src/app/quotes/quotes.component.ts
@@ -8,9 +8,9 @@ import { Quotes } from '../quotes';
 })
 export class QuotesComponent implements OnInit {
   quotes:Quotes[]=[
-    new Quotes("Mahatma Ghandi", 'Priscah Limo', 'Find an online version and watch merlin find his son',5,5 ,new Date(2023,8,4)),
-     new Quotes("Oprah winfrey", 'Priscah Limo', 'Find an online version and watch merlin find his son',5,5 , new Date(2023,8,4)),
-    new Quotes("Maya Angelou", 'Priscah Limo', 'Find an online version and watch merlin find his son',5,5 , new Date(2023,8,4)),
+    new Quotes("Martin Luther King Jr.", 'Priscah Limo', '"Our lives begin to end the day we become silent about things that matter."',5,5 ,new Date(2023,8,4)),
+     new Quotes("Oprah winfrey", 'Priscah Limo', '"Be thankful for what you have; you will end up having more. If you concentrate on what you do not have, you will never, ever have enough"',5,5 , new Date(2023,8,4)),
+    new Quotes("Maya Angelou", 'Priscah Limo', '“I have learned that people will forget what you said, people will forget what you did, but people will never forget how you made them feel.”',5,5 , new Date(2023,8,4)),
   ];
 
 
